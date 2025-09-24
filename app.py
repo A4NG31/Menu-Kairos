0bc260a5d9e9d559edd6b37f8160d8486cccec40
Angel Torres <125419049+A4NG31@users.noreply.github.com>
2025-09-24 15:20:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -536,20 +536,20 @@ st.markdown("""
 # -----------------------------
 st.markdown("""
 <div class="ezytec-section">
-    <h2 class="sub-header">📈 BI Conciliaciones Peajes</h2>
+    <h2 class="sub-header">📈 Menú reportes BI</h2>
     <div class="ezytec-card">
         <div style="text-align: center; margin-bottom: 1.5rem;">
         <img src="https://i.imgur.com/PZFyGpU.png" alt="Cybersource" style="width: 260px; height: 140px;">
         </div>
         <h3 style="color: #FFC900; font-size: 2rem; font-weight: 700; margin-bottom: 1.5rem;">
-            Conciliaciones Peajes
+            REPORTES POWER BI
         </h3>
         <p style="color: #4a5568; font-size: 1.1rem; line-height: 1.7; margin-bottom: 2.5rem;">
             Validación especializada de códigos y transacciones mediante <strong>IA</strong> en la pasarela de pago Cybersource. 
             Asegura la detección de errores y anomalías con algoritmos optimizados para procesos financieros críticos.
         </p>
-        <a href="https://app.powerbi.com/reportEmbed?reportId=a169d1d0-9fe8-4dfe-8271-abfde807dd95&autoAuth=true&ctid=f917ed1b-b403-49c5-b80b-baae3ce0375a" target="_blank">
-            <button class="direct-access-btn ezytec-btn">📈 Acceder al BI de Conciliaciones Peajes</button>
+        <a href="https://app.powerbi.com/view?r=eyJrIjoiNzhjOWEwMzctNmZhYy00NjE1LThjZjctNDVlMjdmMmFlNDlmIiwidCI6ImY5MTdlZDFiLWI0MDMtNDljNS1iODBiLWJhYWUzY2UwMzc1YSJ9" target="_blank">
+            <button class="direct-access-btn ezytec-btn">📈 Acceder al Menu de reportes BI</button>
         </a>
     </div>
 </div>
