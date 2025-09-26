6c4d0534ac306817f920c6a66442a631750b05e4
Angel Torres <125419049+A4NG31@users.noreply.github.com>
2025-09-26 11:12:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -563,9 +563,9 @@ st.markdown("""
     <h2 class="sub-header">🧾 Validador Motores Facturación</h2>
     <div class="ezytec-card">
         <div style="text-align: center; margin-bottom: 1.5rem;">
-        <img src="https://i.imgur.com/VbDfzO5.png" alt="Cybersource" style="width: 260px; height: 140px;">
+        <img src="https://i.imgur.com/VbDfzO5.png" alt="Cybersource" style="width: 200px; height: 140px;">
         </div>
-        <h3 style="color: #FFC900; font-size: 2rem; font-weight: 700; margin-bottom: 1.5rem;">
+        <h3 style="color: #1E3A8A; font-size: 2rem; font-weight: 700; margin-bottom: 1.5rem;">
             MOTORES FACTURACIÓN
         </h3>
         <p style="color: #4a5568; font-size: 1.1rem; line-height: 1.7; margin-bottom: 2.5rem;">
